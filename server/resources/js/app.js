52e31fd18cdeb99a572a37b3a6b9753d36cd943d
StasX <s.mestechkin@gmail.com>
2026-08-12 19:16:14 +0300

    Add frontend setup

--- server/resources/js/app.js
+++ server/resources/js/app.js
@@ -1,1 +1,25 @@
 import './bootstrap';
+import schoolTemplate from '..templates/school.html';
+import administrationTemplate from '..templates/administration.html';
+import notFoundTemplate from '..templates/administration.html';
+
+$(document).ready(function () {
+    $(window).on('hashchange', function () {
+        switch (window.location.hash) {
+            case '/#!school': {
+                $('body').html(schoolTemplate);
+                break;
+            }
+            case '/#!administration': {
+                $('body').html(administrationTemplate);
+                break;
+            }
+            default: {
+                if (window.location.hash) {
+                    $('body').html(notFoundTemplate);
+                }
+            }
+        }
+
+    });
+});
--- server/resources/js/bootstrap.js
+++ server/resources/js/bootstrap.js
@@ -1,4 +1,6 @@
 import $ from 'jquery';
+import Swal from 'sweetalert2';
 
 window.$ = $;
 window.jQuery = $;
+window.Swal = Swal;
