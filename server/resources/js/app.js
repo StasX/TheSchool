7e0e857755426f40616ed10e22c300cf3173e8bc
StasX <s.mestechkin@gmail.com>
2026-08-15 19:51:11 +0300

    Fix school view

--- server/resources/js/app.js
+++ server/resources/js/app.js
@@ -4,38 +4,83 @@ import administrationTemplate from '../templates/administration.html?raw';
 import notFoundTemplate from '../templates/404.html?raw';
 
 $(document).ready(function () {
-    let user;
-    $(window).on('hashchange', function () {
+    let user = null;
+
+    function render() {
         switch (location.hash) {
-            case '#!school': {
+            case '#!school':
                 $('body').html(schoolTemplate);
+
+                console.log('User:', user);
+
+                if (user && (user.Role === 'owner' || user.Role === 'manager')) {
+                    $('#navbar').append(`
+                        <li class="nav-item">
+                            <a href="#!administration" class="nav-link">
+                                Administration
+                            </a>
+                        </li>
+                    `);
+                    $('#user-info').text(`${user.Name}, ${user.Role}`);
+                    $.get(user.Image).done(()=>{
+                        $('#user-image').attr('src', user.Image);
+                    }).fail(()=>{
+                        $('#user-image').attr('src', '/img/user.png');
+                    });
+                }
+
                 break;
-            }
-            case '#!administration': {
+
+            case '#!administration':
                 $('body').html(administrationTemplate);
                 break;
-            }
-            default: {
+
+            default:
                 if (location.hash) {
                     $('body').html(notFoundTemplate);
                 }
-            }
+                break;
         }
-    });
+    }
+
+    $(window).on('hashchange', render);
+
     if (!location.hash) {
         $('#login').on('submit', function (e) {
             e.preventDefault();
-            const data ={
+
+            const data = {
                 Email: $('#user').val(),
                 Password: $('#password').val()
             };
-            $.post('/api/login', data, function (data) {
-                user = data.Administrator;
-                location.hash = '#!school';
-            }).fail(function (xhr) {
-                const error =xhr.status===401 ? 'Invalid username or password' : 'An error occurred. Please try again later.';
-                $('#alerts').html(`<div class="alert alert-danger" role="alert">${error}</div>`);
-            });
+
+            $.post('/api/login', data)
+                .done(function (data) {
+                    console.log(data)
+                    user = data.administrator;
+                    location.hash = '#!school';
+                })
+                .fail(function (xhr) {
+                    const error = xhr.status === 401
+                        ? 'Invalid username or password'
+                        : 'An error occurred. Please try again later.';
+
+                    $('#alerts').html(`
+                        <div class="alert alert-danger" role="alert">
+                            ${error}
+                        </div>
+                    `);
+                });
         });
+    } else {
+        $.get('/api/auth')
+            .done(function (data) {
+                user = data.administrator;
+                render();
+            })
+            .fail(function () {
+                location.hash = '';
+                location.reload();
+            });
     }
 });
