c3f17d320b7290974bcc79b861ce94c60e09c1a3
StasX <s.mestechkin@gmail.com>
2026-08-28 19:54:27 +0300

    Add administrator management to frontend

--- server/resources/js/administration.js
+++ server/resources/js/administration.js
@@ -1,6 +1,11 @@
 import template from '../templates/pages/administration.html?raw';
+import { administratorRender } from './renders/administrator';
 import { userRender } from './renders/user';
+
 export default function administration(user) {
     $('body').html(template);
     userRender(user);
+    $.get('/api/administrator').done((data) => {
+        administratorRender(data);
+    });
 }
