30642ebb26bb8aa3e969f710641b95c45e1770e8
StasX <s.mestechkin@gmail.com>
2026-08-30 18:15:57 +0300

    Refactor logout

--- server/resources/js/renders/user.js
+++ server/resources/js/renders/user.js
@@ -1,18 +1,26 @@
 export function userRender(user) {
-    const navItems=$("#navbar").find(".nav-item");
-    if (user && (user.Role == 'owner' || user.Role == 'manager') && navItems.length == 1) {
-        $('#navbar').append(`
-                            <li class="nav-item">
-                                <a href="#!administration" class="nav-link">
-                                    Administration
-                                </a>
-                            </li>
-                        `);
+    const navbar = $('#navbar');
+    const navItems = navbar.find('.nav-item');
+    const canAdministrate = ['owner', 'manager'].includes(user.Role);
+
+    if (canAdministrate && navItems.length === 1) {
+        navbar.append(`
+            <li class="nav-item">
+                <a href="#!administration" class="nav-link">
+                    Administration
+                </a>
+            </li>
+        `);
     }
     $('#user-info').text(`${user.Name}, ${user.Role}`);
     $('#user-image').attr('src', user.Image);
-    $("#logout").on("click", () => $.post("/api/logout").done(() => {
-        location.hash = '';
-        location.reload();
-    }).fail(xhr => console.error(xhr)));
+    $('#logout').on('click', () => {
+        $.post('/api/logout')
+            .done(() => {
+                location.href='/';
+            })
+            .fail((xhr) => {
+                console.error(xhr);
+            });
+    });
 }
