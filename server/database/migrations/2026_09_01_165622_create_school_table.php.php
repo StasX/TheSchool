e2a5824e9a98296df00bf4cc040dc4d367f44c41
StasX <s.mestechkin@gmail.com>
2026-09-01 20:01:38 +0300

    Fix school table migration

--- server/database/migrations/2026_09_01_165622_create_school_table.php.php
+++ server/database/migrations/2026_09_01_165622_create_school_table.php.php
@@ -6,19 +6,31 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        //
+        Schema::create('school', function (Blueprint $table) {
+            $table->unsignedInteger('Course_ID');
+            $table->unsignedInteger('Student_ID');
+
+            $table->primary([
+                'Course_ID',
+                'Student_ID',
+            ]);
+
+            $table->foreign('Course_ID')
+                ->references('Course_ID')
+                ->on('courses')
+                ->cascadeOnDelete();
+
+            $table->foreign('Student_ID')
+                ->references('Student_ID')
+                ->on('students')
+                ->cascadeOnDelete();
+        });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('school');
     }
 };
