38868aa8c7422698e48fb19c4e3b7c12dad70a60
StasX <s.mestechkin@gmail.com>
2026-09-03 18:06:40 +0300

    Fix Course and Student PHPStan errors

--- server/app/Models/Course.php
+++ server/app/Models/Course.php
@@ -19,6 +19,9 @@ class Course extends Model
         'Image',
     ];
 
+    /**
+     * @return BelongsToMany<Student, $this>
+     */
     public function students(): BelongsToMany
     {
         return $this->belongsToMany(
