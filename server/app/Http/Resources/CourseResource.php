7775165a6dd24d4b3072687f2cd51907ceac33ee
StasX <s.mestechkin@gmail.com>
2026-09-16 16:54:30 +0300

    Fix API resource responses and PHPStan type errors

--- server/app/Http/Resources/CourseResource.php
+++ server/app/Http/Resources/CourseResource.php
@@ -1,23 +1,28 @@
 <?php
+
 namespace App\Http\Resources;
 
+use App\Models\Course;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class CourseResource extends JsonResource
 {
     /**
-     * Transform the resource into an array.
-     *
+     * @var Course
+     */
+    public $resource;
+
+    /**
      * @return array<string, mixed>
      */
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this->Course_ID,
-            'name' => $this->Name,
-            'description' => $this->Description,
-            'image' => $this->Image,
+            'id' => $this->resource->Course_ID,
+            'name' => $this->resource->Name,
+            'description' => $this->resource->Description,
+            'image' => $this->resource->Image,
             'students' => StudentResource::collection(
                 $this->whenLoaded('students')
             ),
