b5357fba4b1fc4b95e26633649de9c731f4542a1
StasX <s.mestechkin@gmail.com>
2026-09-18 16:20:53 +0300

    Fix PHPCS-Fixer issues

--- server/tests/Feature/HealthCheckTest.php
+++ server/tests/Feature/HealthCheckTest.php
@@ -2,15 +2,10 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class HealthCheckTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
     public function test_example(): void
     {
         $response = $this->get('/api/healthcheck');
